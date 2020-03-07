cdaf1de042d5e3f258e3a5f2eac6cadd9a2a6541
Ivan Bosch <ivan.bosch@city.ac.uk>
2020-03-07 01:02:31 +0000

    Coupons now are inserted, but the values of Type, Id and Amount are empty

--- ipm/PHP/coupons_Management.php
+++ ipm/PHP/coupons_Management.php
@@ -5,32 +5,35 @@
 //If not Use the first available blank ID that corresponds to the blank Type choosen
 //Then Insert all the values into the coupons table with the corresponding blank ID
 //But not before creating a ticket entry
-include '../html/header.php';
 
-if (isset($_POST['coupon_Submission'])) {
+if (isset($_POST["coupon_Submission"])) {
   require 'connection.php';
 
-  $coupon_Type = (int)mysqli_real_escape_string($_POST['coupon_Type']);
+  $blank_Type = intval($_POST['blank_Type']);
+  echo "Coupon Type: " . $blank_Type;
   $coupon_Origin = "";
   $coupon_Destination = "";
   $coupon_Date = "";
   $coupon_Time = "";
-  $user_ID = (int)$_POST['id'];
+  $user_ID = intval($_POST['id']);
+  echo "User ID: " . $user_ID;
   $coupon_Number = 1000000;
+  $coupon_Amount = intval($_POST['coupon_Amount']);
+  echo "Coupon Amount: " . $coupon_Amount;
 
   //Generate blank
   $blankSql = "SELECT blank_ID FROM blanks
           WHERE NOT EXISTS (SELECT blank_ID FROM coupons
-          WHERE blanks.blank_ID = coupons.blank_ID) AND blank_Type = $coupon_Type
-          AND blank_Advisor_ID = $user_ID
+          WHERE blanks.blank_ID = coupons.blank_ID) AND blank_Type = '".$blank_Type."'
+          AND blank_Advisor_ID = '".$user_ID."'
           LIMIT 1";
   $result = mysqli_query($db, $blankSql);
-  if ($result <= 0) {
-      header("Location: ../html/generate_Coupons.php?error=noblank");
-      exit();
+  if (mysqli_num_rows($result) == 0) {
+    echo "Blank ID: ". $blankID . "User ID: " . "<br><br>";
+  } else {
+    $row = mysqli_fetch_assoc($result);
+    $blankID = $row['blank_ID'];
   }
-  $blankAssoc = mysqli_fetch_assoc($result);
-  $blankID = (int)$blankAssoc['blank_ID'];
 
   //Create a ticket
   $ticketSql = "INSERT INTO tickets (ticket_ID) VALUES (NULL);";
@@ -41,39 +44,28 @@ if (isset($_POST['coupon_Submission'])) {
                 WHERE tickets.ticket_ID and coupons.ticket_ID)
                 LIMIT 1";
   $ticketResult =$db->query($ticketSql);
-  while ($row = mysqli_fetch_assoc($ticketResult)) {
-    $ticketId = (int)$row['ticket_ID'];
-  }
+  $row = mysqli_fetch_assoc($ticketResult);
+  $ticketId = $row['ticket_ID'];
 
-  for ($y = 0; $y <= $_POST['coupon_Amount']; $y++) {
-    $coupon_Origin = (int)mysqli_real_escape_string($_POST['coupon_Origin'.$y]);
-    $coupon_Destination = (int)mysqli_real_escape_string($_POST['coupon_Destination'.$y]);
-    $coupon_Time = (int)$_POST['coupon_Time'.$y];
-    $coupon_Date = (int)$_POST['coupon_Date'.$y];
+  for ($y = 0 ; $y < $coupon_Amount ; $y++) {
+    $coupon_Origin = mysqli_real_escape_string($db, $_POST['coupon_Origin'.$y.'']);
+    $coupon_Destination = mysqli_real_escape_string($db, $_POST['coupon_Destination'.$y.'']);
+    $coupon_Time = $_POST['coupon_Time'.$y.''];
+    $coupon_Date = $_POST['coupon_Date'.$y.''];
 
-    //Problem might be Time and date, the script goes through and is coupon_Submission_Successful
-    //It is a sql thing that coupon doesn't accept my variables
-    //The ID's are int(10), origin and destination are varchar(30), coupon number int(8)
-    //Time and date their respective type Time and Date
-    $sql = "INSERT INTO coupons (blank_ID, ticket_ID, coupon_Origin, coupon_Destination, coupon_Time, coupon_Date, coupon_Number)
-            VALUES (?, ?, ?, ?, $coupon_Time, $coupon_Date, ?)";
+    //Insertion into coupons
+    $sql = "INSERT INTO coupons (blank_ID, ticket_ID, coupon_Origin, coupon_Destination, coupon_Time, coupon_Date)
+           VALUES ('".$blankID."', '".$ticketId."', '".$coupon_Origin."', '".$coupon_Destination."', '".$coupon_Time."', '".$coupon_Date."')";
+    $result = mysqli_query($db, $sql);
 
-    $stmt = mysqli_stmt_init($db);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-      header("Location: ../html/generate_Coupons.php?error=sqlerror=coupon_submission");
-      exit();
-    }
-    else {
-      mysqli_stmt_bind_param($stmt, "iissi", $blankID, $ticketId, $coupon_Origin, $coupon_Destination, $coupon_Number);
-      mysqli_stmt_execute($stmt);
+  echo "Blank ID: ". $blankID . ' Ticket ID: ' . $ticketId . ' Origin: ' . $coupon_Origin . ' Destination: ' . $coupon_Destination . ' Time: ' . $coupon_Time . ' Date: ' . $coupon_Date;
 
-      header("Location: ../html/sales.html?coupon_Submission_Successful");
-      exit();
-    }
+  //   header("Location: ../html/coupon.html?coupon_Submission_Successful");
+  //   exit();
   }
 }
 else {
-  header("Location: ../html/generate_Coupons.php?error");
+  header("Location: ../html/generate_Coupons.php?error=unauthorizedaccess");
   exit();
 }
 ?>
--- ipm/html/generate_Coupons.php
+++ ipm/html/generate_Coupons.php
@@ -6,16 +6,17 @@
     <title></title>
 
     <script type="text/javascript">
+      //Function that populates the form according to the decision of the Blank Type
       function chooseType(){
         let generatedHTML ="";
-        const selection = document.getElementById("coupon_Type").value;
+        const selection = document.getElementById("blank_Type").value;
         switch (selection) {
           case '420':
             let j = 0;
             for (i = 0; i < 2; i++){
               document.getElementById("coupon_Amount").innerHTML = '<input type="hidden" id="coupon_Amount" name="coupon_Amount" value="2"/>'
               generatedHTML += `
-                  <form class='generate_Coupons' action='../PHP/coupons.php' method='post'>
+                  <form class='generate_Coupons' action='../PHP/coupons_Management.php' method='post'>
                     <input type='text' name='coupon_Origin${i}' placeholder='Enter Origin..'>
                     <input type='text' name='coupon_Destination${i}' placeholder='Enter Destination..'><br>
                     <label for='coupon_Date'>Select Coupon Date:</label>
@@ -23,7 +24,6 @@
                     <label for='coupon_Time'>Select Coupon Time:</label>
                     <input type='time' name='coupon_Time${i}' id='coupon_Time'>
                   </form><br>`;
-              document.getElementById("coupon_Amount").value = i
             }
             document.getElementById("0").innerHTML = generatedHTML;
             document.getElementById("0").innerHTML += '<button type="submit" id="submit" name="coupon_Submission">Submit Coupons</button>';
@@ -33,7 +33,7 @@
             for (i = 0; i < 2; i++){
               document.getElementById("coupon_Amount").innerHTML = '<input type="hidden" id="coupon_Amount" name="coupon_Amount" value="2"/>'
               generatedHTML += `
-                  <form class='generate_Coupons' action='../PHP/coupons.php' method='post'>
+                  <form class='generate_Coupons' action='../PHP/coupons_Management.php' method='post'>
                     <input type='text' name='coupon_Origin${i}' placeholder='Enter Origin..'>
                     <input type='text' name='coupon_Destination${i}' placeholder='Enter Destination..'><br>
                     <label for='coupon_Date'>Select Coupon Date:</label>
@@ -41,7 +41,6 @@
                     <label for='coupon_Time'>Select Coupon Time:</label>
                     <input type='time' name='coupon_Time${i}' id='coupon_Time'>
                   </form><br>`;
-              document.getElementById("coupon_Amount").value = i
             }
             document.getElementById("0").innerHTML = generatedHTML;
             document.getElementById("0").innerHTML += '<button type="submit" id="submit" name="coupon_Submission">Submit Coupons</button>';
@@ -50,8 +49,9 @@
           case '101':
             for (i = 0; i < 1; i++){
               document.getElementById("coupon_Amount").innerHTML = '<input type="hidden" id="coupon_Amount" name="coupon_Amount" value="1"/>'
+              console.log(document.getElementById("blank_Type").value);
               generatedHTML += `
-                  <form class='generate_Coupons' action='../PHP/coupons.php' method='post'>
+                  <form class='generate_Coupons' action='../PHP/coupons_Management.php' method='post'>
                     <input type='text' name='coupon_Origin${i}' placeholder='Enter Origin..'>
                     <input type='text' name='coupon_Destination${i}' placeholder='Enter Destination..'><br>
                     <label for='coupon_Date'>Select Coupon Date:</label>
@@ -59,7 +59,6 @@
                     <label for='coupon_Time'>Select Coupon Time:</label>
                     <input type='time' name='coupon_Time${i}' id='coupon_Time'>
                   </form><br>`;
-              document.getElementById("coupon_Amount").value = i
             }
             document.getElementById("0").innerHTML = generatedHTML;
             document.getElementById("0").innerHTML += '<button type="submit" id="submit" name="coupon_Submission">Submit Coupons</button>';
@@ -67,9 +66,10 @@
 
           case '444':
             document.getElementById("coupon_Amount").innerHTML = '<input type="hidden" id="coupon_Amount" name="coupon_Amount" value="4"/>'
+            console.log(document.getElementById("coupon_Amount").value);
             for (i = 0; i < 4; i++){
               generatedHTML += `
-                  <form class='generate_Coupons' action='../PHP/coupons.php' method='post'>
+                  <form class='generate_Coupons' action='../PHP/coupons_Management.php' method='post'>
                     <input type='text' name='coupon_Origin${i}' placeholder='Enter Origin..'>
                     <input type='text' name='coupon_Destination${i}' placeholder='Enter Destination..'><br>
                     <label for='coupon_Date'>Select Coupon Date:</label>
@@ -77,7 +77,6 @@
                     <label for='coupon_Time'>Select Coupon Time:</label>
                     <input type='time' name='coupon_Time${i}' id='coupon_Time'>
                   </form><br>`;
-              document.getElementById("coupon_Amount").value = i
             }
             document.getElementById("0").innerHTML = generatedHTML;
             document.getElementById("0").innerHTML += '<button type="submit" id="submit" name="coupon_Submission">Submit Coupons</button>';
@@ -88,9 +87,9 @@
   </head>
 
   <body>
-    <form method="post">
-      <label for="coupon_Type">Choose Blank Type:</label>
-      <select id="coupon_Type" name="blanks">
+    <form>
+      <label for="blank_Type">Choose Blank Type:</label>
+      <select id="blank_Type" name="blank_Type">
         <option value="444">444</option>
         <option value="420">420</option>
         <option value="201">201</option>
@@ -98,10 +97,8 @@
       </select>
       <button type="button" onclick="chooseType();">Choose Type</button><br>
     </form>
-    <input type="hidden" id="coupon_Amount" name="coupon_Amount"/>
-    <form method="post" action="../PHP/coupons_Management.php">
-      <p id="0"></p>
-    </form>
+    <input type="hidden" id="coupon_Amount" value="1"/>
+    <form method="post" id="0" action="../PHP/coupons_Management.php"></form>
     <?php echo "<input type='hidden' value='".$_SESSION['id']."' name='id'" ?>
     <?php echo "<h1>hello".$_SESSION['id']."</h1>" ?>
   </body>
