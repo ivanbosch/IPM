833301648398cac05670ae1102c1c97a4a3af7ab
Ivan Bosch <ivan.bosch@city.ac.uk>
2020-04-05 12:30:18 +0100

    Refunds added to sales

---
+++ ipm/PHP/refund.php
@@ -0,0 +1,41 @@
+<?php
+require "../PHP/connection.php";
+session_start();
+
+if (isset($_POST['refund_Submission'])) {
+  $refundID = $_POST['refundID'];
+  $file = 'refund_Log.txt';
+
+  //Get the sale
+  $query = $db->query("SELECT * FROM sales WHERE ticket_ID = '$refundID'");
+  $result = $query->fetch_assoc();
+
+  $ticketID = $result['ticket_ID'];
+  $customerID = $result['customer_ID'];
+  $saleID = $result['sales_ID'];
+
+  //Customer Details
+  $query_customer = $db->query("SELECT customer_Name AS name, customer_Surname AS surname, customer_Email AS email FROM customers WHERE customer_ID='$customerID'");
+  $customer = $query_customer->fetch_assoc();
+
+  //String saved to file
+  $string = "Sale ID: ".$saleID." Type: ".$result['sales_Type']." Customer Refunded: ".$customer['name']." ".$customer['surname']
+            ." Customer Email: ".$customer['email']. " Amount Refunded: ".$result['sales_Charge']." Refund made by:".$_SESSION['id']."\n";
+
+  //Open, Write and Close file
+  $handle = fopen($file, 'a') or die("Unable to open file!");
+  fwrite($handle, $string);
+  fclose($handle);
+
+  //Delete from DB to make the blanks available again
+  $db->query("DELETE FROM sales WHERE sales_ID=$saleID");
+  $db->query("DELETE FROM coupons WHERE ticket_ID=$ticketID");
+  $db->query("DELETE FROM tickets WHERE ticket_ID=$ticketID");
+
+  header("Location: ../html/sales.php?error=unauthorizedaccess");
+  exit();
+
+} else {
+  header("Location: ../html/sales.php?error=unauthorizedaccess");
+  exit();
+}
